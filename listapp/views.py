d26101e289756989c633404eca242782c91cc24e
marutaro1 <usersrecord@gmail.com>
2024-06-26 07:54:24 +0900

    setting signupfunc

--- listapp/views.py
+++ listapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        print(request.POST)
+        password = request.POST['password']
+        user = User.objects.create_user(username,'',password)
     return render(request,'signup.html',{'sam':'samdata'})
