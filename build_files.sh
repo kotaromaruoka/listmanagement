c6c09cab9faa01472be7d7214c4ab0af31b54408
marutaro1 <usersrecord@gmail.com>
2024-07-11 07:38:15 +0900

    update

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Ensure pip is installed
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, installing now..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3.9 get-pip.py
+    rm get-pip.py
+fi
+
 # Install dependencies
 python3.9 -m pip install -r requirements.txt
 
-# Apply migrations (if applicable)
+# Apply migrations
 python3.9 manage.py migrate
 
-# Collect static files (if applicable)
+# Collect static files
 python3.9 manage.py collectstatic --noinput
