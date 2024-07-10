60bddea30fd3c876919a0f4065712fe21a4cd580
marutaro1 <usersrecord@gmail.com>
2024-07-11 07:34:57 +0900

    update

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Apply migrations (if applicable)
+python3.9 manage.py migrate
+
+# Collect static files (if applicable)
+python3.9 manage.py collectstatic --noinput
