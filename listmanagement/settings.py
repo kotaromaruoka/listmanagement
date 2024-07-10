20a79e9d0f6a5eb5b8942b9ca6e2d749b7a3f1df
marutaro1 <usersrecord@gmail.com>
2024-07-11 07:12:15 +0900

    deploy setting

--- listmanagement/settings.py
+++ listmanagement/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = "django-insecure-i0u(!8i!ypm==lyn&+y(px$un9sme8*b)k0h0tkrocgv3h4imu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+    '.vercel.app',
+    ]
 
 
 # Application definition
@@ -99,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
