c4eae176b15449b1ad60a1eb9751fac5f3c6802d
marutaro1 <usersrecord@gmail.com>
2024-07-11 08:42:11 +0900

    update

--- listmanagement/settings.py
+++ listmanagement/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        # 'NAME': '/tmp/db.sqlite3',
+        # "NAME": BASE_DIR / "db.sqlite3",
+        'NAME': '/tmp/db.sqlite3',
     }
 }
 
