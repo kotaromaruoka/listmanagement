b808106d2ba45f242d48d93e7f87677aabc90ba2
marutaro1 <usersrecord@gmail.com>
2024-07-11 10:24:33 +0900

    db setting mysql

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# pip のインストールを確認する
+# Ensure pip is installed
 if ! command -v pip &> /dev/null
 then
-    echo "pip が見つかりません、インストールします..."
+    echo "pip could not be found, installing now..."
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3.9 get-pip.py
     rm get-pip.py
 fi
 
-# 依存パッケージのインストール
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
 
-# マイグレーションの適用
-python3.9 manage.py migrate --noinput
+# Apply migrations
+python3.9 manage.py migrate
 
-# 静的ファイルの収集
+# Set correct permissions for the SQLite database
+chmod 664 /tmp/db.sqlite3
+chown www-data:www-data /tmp/db.sqlite3
+
+
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
