8ae24db2e78f1a572fce387b8d93ebf65830ad70
marutaro1 <usersrecord@gmail.com>
2024-07-11 10:49:51 +0900

    Return to mysql

--- build_files.sh
+++ build_files.sh
@@ -11,7 +11,8 @@ fi
 
 # Install dependencies
 python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install django
+python3.9 -m pip install django==4.2.13
+python3.9 -m pip install mysqlclient
 
 python3.9 manage.py makemigrations
 
