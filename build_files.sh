51a0998bc18be5bd6760844888a1995bddd1f3d0
marutaro1 <usersrecord@gmail.com>
2024-07-11 09:44:49 +0900

    db setting mysql

--- build_files.sh
+++ build_files.sh
@@ -12,8 +12,8 @@ fi
 # Install dependencies
 python3.9 -m pip install -r requirements.txt
 
+# Apply migrations
 python3.9 manage.py migrate
-
 
 # Collect static files
 python3.9 manage.py collectstatic --noinput
