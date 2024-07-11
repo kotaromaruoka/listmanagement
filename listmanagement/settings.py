873ad3e7a20de2e461c9d2f5a8eadb77728bc1a0
marutaro1 <usersrecord@gmail.com>
2024-07-11 14:20:17 +0900

    Return to mysql

--- listmanagement/settings.py
+++ listmanagement/settings.py
@@ -95,9 +95,16 @@ DATABASES = {
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
         'HOST': os.environ.get('MYSQL_HOST'),
-        'PORT': os.environ.get('MYSQL_PORT'),
-    }
+        'PORT': os.environ.get('MYSQL_PORT','3306'),
+    },
+    'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+    },
 }
+
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Password validation
