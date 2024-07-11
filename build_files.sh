88f1e64423e061d6326da4c75daf46b8eaac212c
marutaro1 <usersrecord@gmail.com>
2024-07-11 14:05:31 +0900

    Return to mysql

--- build_files.sh
+++ build_files.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-
-# Ensure pip is installed
-if ! command -v pip &> /dev/null
-then
-    echo "pip could not be found, installing now..."
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3.9 get-pip.py
-    rm get-pip.py
-fi
-
-apt-get update && apt-get install -y \
-    python3-dev \
-    default-libmysqlclient-dev \
-    build-essential
-
-# Install dependencies
-python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install django==4.2.13
-
-
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate  
+pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput
+python3　manage.py collectstatic --noinput
