b532ba297d80dc2b643d245eb276bbd691119fa2
marutaro1 <usersrecord@gmail.com>
2024-07-11 10:06:50 +0900

    db setting mysql

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Ensure pip is installed
+# pip のインストールを確認する
 if ! command -v pip &> /dev/null
 then
-    echo "pip could not be found, installing now..."
+    echo "pip が見つかりません、インストールします..."
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3.9 get-pip.py
     rm get-pip.py
 fi
 
-# Install dependencies
+# 依存パッケージのインストール
 python3.9 -m pip install -r requirements.txt
 
-# Apply migrations
-python3.9 manage.py migrate
+# マイグレーションの適用
+python3.9 manage.py migrate --noinput
 
-# Collect static files
+# 静的ファイルの収集
 python3.9 manage.py collectstatic --noinput
